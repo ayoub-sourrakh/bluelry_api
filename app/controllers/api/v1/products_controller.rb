224ea4e544cd8dcc9d2e48ea10140404aeb7eae4
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-16 00:42:36 +0200

    app/controllers/api/v1/products_controller.rb

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,20 +1,31 @@
+# app/controllers/api/v1/products_controller.rb
 module Api
-  module V1
-    class ProductsController < ApplicationController
-      def create
-        product = Product.new(product_params)
-        if product.save
-          render json: { status: 'SUCCESS', message: 'Product created', data: product }, status: :ok
-        else
-          render json: { status: 'ERROR', message: 'Product not created', data: product.errors }, status: :unprocessable_entity
+    module V1
+      class ProductsController < ApplicationController
+        def create
+          product = Product.new(product_params)
+          if product.save
+            render json: { status: 'SUCCESS', message: 'Product created', data: product }, status: :ok
+          else
+            render json: { status: 'ERROR', message: 'Product not created', data: product.errors }, status: :unprocessable_entity
+          end
         end
-      end
-
-      private
-
-      def product_params
-        params.require(:product).permit(:name, :description, :price)
+  
+        def show
+          product = Product.find_by(id: params[:id])
+          if product
+            render json: { status: 'SUCCESS', message: 'Loaded product', data: product }, status: :ok
+          else
+            render json: { status: 'ERROR', message: 'Product not found', data: {} }, status: :not_found
+          end
+        end
+  
+        private
+  
+        def product_params
+          params.require(:product).permit(:name, :description, :price)
+        end
       end
     end
   end
-end
+  
