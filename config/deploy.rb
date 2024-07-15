990ce846955b22c99be6b58394a2eaf718cef955
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-15 12:11:06 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,16 +19,4 @@ set :keep_releases, 5
 
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 
-# Puma configuration
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
 set :branch, 'main'
