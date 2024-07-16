16aa4af1907575880bae8beeda0d5fba64a0ccce
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-16 15:42:53 +0200

    user secure regisration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :index, :show, :update, :destroy]
