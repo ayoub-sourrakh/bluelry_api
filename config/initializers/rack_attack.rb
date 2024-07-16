32bd4dbc522ee328665053843d6bb0446fa6bef8
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-16 15:29:03 +0200

    config/initializers/rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 # config/initializers/rack_attack.rb
 class Rack::Attack
-  throttle('req/ip', limit: 60, period: 1.minute) do |req|
+  throttle('req/ip', limit: 10, period: 1.minute) do |req|
     req.ip
   end
 end
