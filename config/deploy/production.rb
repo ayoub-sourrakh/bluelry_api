52d8ff4cb90aa760dc3e0d271a11f0b8a29bb233
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-17 00:45:53 +0200

    config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '35.180.205.58', user: 'ubuntu', roles: %w{app db web}
+server '13.37.32.243', user: 'ubuntu', roles: %w{app db web}
 
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
