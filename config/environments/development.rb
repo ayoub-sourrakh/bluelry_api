b98537e588b21f52246f392483710e5ebc66124c
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-07-17 02:01:23 +0200

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.force_ssl = true
+  # config.force_ssl = true
   
   config.action_dispatch.default_headers = {
     'X-Frame-Options' => 'SAMEORIGIN',
