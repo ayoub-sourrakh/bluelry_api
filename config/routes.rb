e9e6b727db6c882ffdfc03493521dca4e5cd28ab
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-12 22:41:17 +0200

    route register

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :index, :show, :update, :destroy]
+
+      resources :users, only: [:create]
     end
   end
 end
