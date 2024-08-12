811c63f3eb198f7ca2149237f132e25ef308acc5
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-12 23:38:20 +0200

    register

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :index, :show, :update, :destroy]
+
+      devise_scope :user do
+        post 'register', to: 'users/registrations#create'
+      end
     end
   end
 end
