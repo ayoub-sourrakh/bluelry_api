72bd5140cf0868ad158ff614b282d5397bda6ffe
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-13 00:41:23 +0200

    app/controllers/

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,27 +1,18 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   def create
-    build_resource(sign_up_params)
-
-    resource.save
-    if resource.errors.any?
-      Rails.logger.info("Validation errors: #{resource.errors.full_messages.join(', ')}")
-    end
-
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
+    Rails.logger.info("Params received: #{params.inspect}")
+  
+    # Prendre uniquement les paramètres sous "user"
+    user_params = params.require(:user).permit(:email, :password, :password_confirmation)
+    
+    # Construire la ressource utilisateur avec ces paramètres
+    build_resource(user_params)
+  
+    if resource.save
+      render json: { status: 'success', user: resource }, status: :created
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      Rails.logger.info("Validation errors: #{resource.errors.full_messages.join(', ')}")
+      render json: { status: 'error', errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
