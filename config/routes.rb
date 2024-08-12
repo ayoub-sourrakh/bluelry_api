033e05801cba6f0d6946c91654284afa6bccc74e
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-12 23:05:26 +0200

    regiter

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-
+  
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :index, :show, :update, :destroy]
-
-      devise_scope :user do
-        post 'register', to: 'api/v1/registrations#create'
-      end
     end
   end
 end
