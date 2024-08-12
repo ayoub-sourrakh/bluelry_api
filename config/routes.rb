859cebd7fae43ae265dba96c4b7164525d7d21b5
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-12 23:00:01 +0200

    registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  
+
   namespace :api do
     namespace :v1 do
       resources :products, only: [:create, :index, :show, :update, :destroy]
 
       devise_scope :user do
-        post 'register', to: 'registrations#create'
+        post 'register', to: 'api/v1/registrations#create'
       end
     end
   end
