4381f3109812548a9d40b3fb6fd031825164ce5e
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-12 23:24:53 +0200

    register

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-  # Route pour le frontend React
-  root 'home#index' # Redirige vers le contrôleur qui sert l'application React
-
-  # Routes pour l'API versionnée
   namespace :api do
     namespace :v1 do
-      devise_for :users, skip: [:registrations, :passwords], controllers: {
-        sessions: 'api/v1/sessions'
+      # Devise routes pour l'authentification
+      devise_for :users, skip: [:passwords], controllers: {
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions'
       }
 
-      # Autres routes de l'API ici, par exemple :
+      # Routes pour d'autres ressources de l'API
       resources :products, only: [:index, :show, :create, :update, :destroy]
     end
   end
-
-  # Route pour gérer toutes les autres requêtes non API et les rediriger vers React
-  get '*path', to: 'home#index', constraints: ->(req) { req.path.exclude? 'api/v1' }
 end
