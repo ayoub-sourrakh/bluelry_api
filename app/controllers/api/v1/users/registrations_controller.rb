5ae691f629d495a4d513ea1c48b46e1dfb49ef67
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-13 00:56:19 +0200

    register add name

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,18 +1,22 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+  
   def create
-    Rails.logger.info("Params received: #{params.inspect}")
-  
-    # Prendre uniquement les paramètres sous "user"
-    user_params = params.require(:user).permit(:email, :password, :password_confirmation)
-    
-    # Construire la ressource utilisateur avec ces paramètres
+    user_params = sign_up_params
+
     build_resource(user_params)
-  
+
     if resource.save
       render json: { status: 'success', user: resource }, status: :created
     else
       Rails.logger.info("Validation errors: #{resource.errors.full_messages.join(', ')}")
       render json: { status: 'error', errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :date_of_birth)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_15_225859) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_12_224603) do
   create_table "jwt_denylists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "jti"
     t.datetime "created_at", null: false
@@ -34,6 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_15_225859) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
