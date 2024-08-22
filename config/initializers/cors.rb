55b0dfc29ed2e366fb53fc61ad3638317a54f52a
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-22 02:51:23 +0200

    fix profile page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'bluelry.com', 'www.bluelry.com', 'https://bluelry.com', 'https://www.bluelry.com', '127.0.0.1'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+  allow do
+    origins 'bluelry.com', 'www.bluelry.com', 'https://bluelry.com', 'https://www.bluelry.com', '127.0.0.1'
+
+    resource '*',
+      headers: :any,
+      expose: ['Authorization'],  # This line exposes the Authorization header
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
-  
+end
