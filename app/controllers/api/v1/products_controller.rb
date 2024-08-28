7ad5f56713d8b3ba2d40dc53f4eafe149855af6c
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-29 01:39:58 +0200

    fix*

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,6 @@
 module Api
     module V1
         class ProductsController < ApplicationController
-            skip_before_action :authenticate_user!, only: [:index, :show]
 
             def index
                 products = Product.all
