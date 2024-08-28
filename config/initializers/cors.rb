771bd3d9a782ad43ba0b890e51e54f9fa19610cc
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-29 01:21:21 +0200

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'bluelry.com', 'www.bluelry.com', 'https://bluelry.com', 'https://www.bluelry.com'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true  # Set to true if you need cookies to be sent with requests
-    end
+  allow do
+    origins 'bluelry.com', 'www.bluelry.com', 'https://bluelry.com', 'https://www.bluelry.com', '127.0.0.1', 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      expose: ['Authorization'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
-  
+end
