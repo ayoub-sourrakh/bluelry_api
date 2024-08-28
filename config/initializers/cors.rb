3288309c90c67b68bab634aaeb4ea4acf7637986
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-08-29 01:47:05 +0200

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      expose: ['Authorization'],  # This line exposes the Authorization header
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
