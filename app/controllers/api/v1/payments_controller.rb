9f4304bfb243238d51459fc0f1e602de4b83bcf9
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-09-02 19:25:22 +0200

    fix

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,19 +1,22 @@
 # app/controllers/payments_controller.rb
-class PaymentsController < ApplicationController
-    def create_payment_intent
-      amount = params[:amount]
-  
-      begin
-        payment_intent = Stripe::PaymentIntent.create(
-          amount: amount,
-          currency: 'eur', # Set your currency
-          payment_method_types: ['card'],
-        )
-  
-        render json: { client_secret: payment_intent.client_secret }, status: :ok
-      rescue Stripe::StripeError => e
-        render json: { error: e.message }, status: :unprocessable_entity
-      end
+module Api
+    module V1
+        class PaymentsController < ApplicationController
+            def create_payment_intent
+            amount = params[:amount]
+        
+            begin
+                payment_intent = Stripe::PaymentIntent.create(
+                amount: amount,
+                currency: 'eur', # Set your currency
+                payment_method_types: ['card'],
+                )
+        
+                render json: { client_secret: payment_intent.client_secret }, status: :ok
+            rescue Stripe::StripeError => e
+                render json: { error: e.message }, status: :unprocessable_entity
+            end
+            end
+        end
     end
-  end
-  
+end
