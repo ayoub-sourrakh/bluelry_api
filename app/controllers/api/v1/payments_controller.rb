e42d16492fbf234a9f9748038ffe19210c72429f
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-09-02 19:44:04 +0200

    fix

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,29 +1,33 @@
 module Api
     module V1
-      class PaymentsController < ApplicationController
-        before_action :authenticate_api_v1_user!
-
-        def create_payment_intent
-          amount = params[:amount]
-  
-          if amount.nil?
-            render json: { error: "Amount is missing" }, status: :unprocessable_entity
-            return
-          end
-  
-          begin
-            payment_intent = Stripe::PaymentIntent.create(
-              amount: amount,
-              currency: 'eur',
-              payment_method_types: ['card'],
-            )
-  
-            render json: { client_secret: payment_intent.client_secret }, status: :ok
-          rescue Stripe::StripeError => e
-            render json: { error: e.message }, status: :unprocessable_entity
-          end
+        class PaymentsController < ApplicationController
+            before_action :authenticate_api_v1_user!
+            
+            def create_payment_intent
+                Rails.logger.info("Starting create_payment_intent action")
+                Rails.logger.info("Received params: #{params.inspect}")
+                
+                amount = params[:amount]
+                Rails.logger.info("Amount to process: #{amount}")
+                
+                if amount.nil?
+                    Rails.logger.error("Amount is missing from the request")
+                    render json: { error: "Amount is missing" }, status: :unprocessable_entity
+                    return
+                end
+                
+                begin
+                    payment_intent = Stripe::PaymentIntent.create(
+                    amount: amount,
+                    currency: 'eur',
+                    payment_method_types: ['card'],
+                    )
+                    
+                    render json: { client_secret: payment_intent.client_secret }, status: :ok
+                rescue Stripe::StripeError => e
+                    render json: { error: e.message }, status: :unprocessable_entity
+                end
+            end
         end
-      end
     end
-  end
-  
+end
