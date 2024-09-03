57e25b4789bc395d903a2a41b17a8bc265a3fb17
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-09-03 03:17:53 +0200

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_bluelry_session', domain: :all, tld_length: 2, secure: Rails.env.production?
-
+Rails.application.config.session_store :cookie_store, key: '_bluelry_session'
