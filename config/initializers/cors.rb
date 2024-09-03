f6ae0321021ab91a9330feadcf977c83371da6a9
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2024-09-03 03:00:29 +0200

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'bluelry.com', 'www.bluelry.com', 'https://bluelry.com', 'https://www.bluelry.com', '127.0.0.1', 'http://localhost:3000'
+    origins 'bluelry.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
